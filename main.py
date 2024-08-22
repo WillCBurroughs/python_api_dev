44343c6b664ebc2be27e5827f638335a2f7e3db4
WillCBurroughs <willcburroughs@gmail.com>
2024-08-21 20:33:40 -0400

    Feature: importing basemodel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,4 +15,7 @@ user_name = "Hello world"
 async def return_user_name():
     return user_name
 
-
+@app.post("/createpost")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new post": f"title {payload['title']} content: {payload['content']}"}
